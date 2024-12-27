bd236bda65f6ee314989acf28c6b16820e0620dc
Sidhartha Veldhandi <76509525+SIDDHU123M@users.noreply.github.com>
2024-12-27 00:50:31 +0000

    dun

--- reactThings/react-app/src/App.jsx
+++ reactThings/react-app/src/App.jsx
@@ -1,26 +1,57 @@
-import { useState } from "react";
-import List from "./List.jsx"
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function Button() {
+// function Counter() {
+//   let [Count, setCount] = useState(0);
+//   return (
+//     <>
+//       <div className="flex">
+//         <h3>Counter : {Count}</h3>
+//         <div className="Button">
+//           <button onClick={() => setCount(Count + 1)}>+</button>
+//           <button onClick={() => setCount(0)}>Reset</button>
+//           <button onClick={() => setCount(Count - 1)}>-</button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
-  let [count, setCount] = useState(0)
-  function increment() {
-    setCount(count+1)
-  }
+// function ColorPicker() {
+//   const [color, setColor] = useState('#FFFFFF')
 
-  return (
-  <button onClick={increment}>Clicked {count} times</button>
-)
-}
+//   return (<div className="colorPicker">
+//     <h3>Color Picker</h3>
+//     <div className="colorDIV" style={{backgroundColor: color}}> Selected Color: {color}</div>
+
+//     <label>Select Color </label>
+//     <input type="color" onChange={(e) => setColor(e.target.value)}/>
+//   </div>)
+// }
+
+// function Foods() {
+//   const [Food, setFood] = useState(['Apple', 'Banana', 'Mango'])
+
+//   return(<>
+//   <h3>Food Listing</h3>
+//     <ul>
+//       {Food.map((element, index) => <li key={index} onClick={() => {
+//         setFood(Food.filter((_,i) => i !== index) )
+//       }}>{element}</li>)}
+//     </ul>
+//     <input type="text" id='addFood'/>
+//     <button onClick={() => {
+//       let value = document.getElementById('addFood').value
+//       document.getElementById("addFood").value = '';
+//       setFood(() => [...Food, value])
+//       }}>Add</button>
+//   </>)
+// }
 
 export default function App() {
-  let items = [{name: "Python", Ext: ".py"}, {name: "JavaScript", Ext: ".js"}, {name: "TypeScript", Ext: ".ts"}];
   return (
     <>
-      <List items={items} heading="Programing Languages"/>
-      <Button/>
-      <Button/>
+      
     </>
   );
 }
-
